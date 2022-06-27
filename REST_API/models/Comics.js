48bbedcfd7fa6702020b1a18c86c17018c4e653c
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-06-27 11:24:05 +0300

    create model comics

---
+++ REST_API/models/Comics.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const comicsSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  price: { type: Number, default: true },
+  year: { type: Number, required: true },
+  userLiked: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  author: { type: String, required: true },
+});
+
+const Comics = mongoose.model("Comics", comicsSchema);
+
+module.exports = Comics;
