fed053037baf631b9faa60d0e94950d78d82a74d
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-06-27 11:21:32 +0300

    add home controller

---
+++ REST_API/controllers/homeController.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const dataServices = require("../services/orderServices");
+const comicServices = require("../services/comicsServices");
+
+router.get("/", async (req, res) => {
+  try {
+    let data = await comicServices.getAllComics();
+    res.json(data);
+  } catch (error) {
+    res.status(400).json({ message: "Bad request" });
+  }
+
+  console.log("rest api get");
+});
+
+module.exports = router;
