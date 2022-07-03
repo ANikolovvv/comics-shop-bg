441072fd3c7055fbdae391388034863d6012140f
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-07-03 20:51:32 +0300

    add spiner css

---
+++ SPA/src/components/Spinner/Spinner.js
@@ -0,0 +1,18 @@
+import "./Spinner.css";
+
+export const Spinner = () => {
+  return (
+    <div className="center">Loading...
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+    </div>
+  );
+};
