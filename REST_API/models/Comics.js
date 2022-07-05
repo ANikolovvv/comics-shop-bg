6e9d34c8165de90da528d9175cda630d8c1de8a9
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-07-05 21:21:39 +0300

    Update Comics.js

--- REST_API/models/Comics.js
+++ REST_API/models/Comics.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const comicsSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  description: { type: String, required: true },
+  description: { type: String},
   imageUrl: { type: String, required: true },
   price: { type: Number, default: true },
   year: { type: Number, required: true },
