84a5e2832ddaa8cca23a45cfeb5007be76613439
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-07-05 21:21:59 +0300

    Update order model

--- REST_API/models/Order.js
+++ REST_API/models/Order.js
@@ -3,12 +3,15 @@ const mongoose = require("mongoose");
 const orderSchema = new mongoose.Schema({
   title: { type: String, required: [true, "All fields are required"] },
   author: { type: String, required: true },
-  year: { type: Number, required: true },
-  image: { type: String, required: true },
+  email: { type: String, required: true },
+  address: { type: String, required: true },
+  courier: { type: String, required: true },
+  number:{type:Number,required:true},
+  payment: { type: String, required: true ,enum:['cash-delivery','credit-card','debit-card']},
   price: { type: Number, default: true },
   _ownerId: { type: mongoose.Types.ObjectId, ref: "User" },
 });
-
+///    const ctx = { title, author, email, address,courier,payment };
 const Order = mongoose.model("Order", orderSchema);
 
 module.exports = Order;
