bf68239054b504b60608d70a2d5033d34d6783ee
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-07-05 21:22:05 +0300

    Update User.js

--- REST_API/models/User.js
+++ REST_API/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
-  orderHistory: [{ type: mongoose.Types.ObjectId, ref: "Orders" }],
+  orderHistory: [{ type: mongoose.Types.ObjectId, ref: "Order" }],
 });
 const User = mongoose.model("User", userSchema);
 module.exports = User;
