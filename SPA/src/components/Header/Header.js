da53847e09c134ba265b793bcb6f9762fa0ab9f2
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-07-09 18:45:10 +0300

    Update header

--- SPA/src/components/Header/Header.js
+++ SPA/src/components/Header/Header.js
@@ -3,73 +3,68 @@ import AuthContexts from "../../contexts/authContext";
 import { useContext } from "react";
 import { resLogout } from "../../services/server";
 
-
-import './Header.css'
+import styles from "./Header.module.css";
 
 const Header = (props) => {
   console.log("props", props);
 
-   let [user, setContext] = useContext(AuthContexts);
-  const logoutHandller = (e) => {
-    //let out = resLogout();
-   
-    e.preventDefault();
-    let token=JSON.parse(localStorage.getItem('user'));
-      console.log(localStorage.getItem('user'),'sdsaassasaaaaaaaaa')
-     resLogout(token.accessToken)
-     localStorage.clear();
+  let [user, setContext] = useContext(AuthContexts);
+  console.log(localStorage.getItem("user"), "sdsaassasaaaaaaaaa");
+  const token = JSON.parse(localStorage.getItem("user"));
+  const logoutHandller = () => {
+    
+    console.log(token.accessToken,'logaout')
+    resLogout(token.accessToken);
+    localStorage.clear();
     user = undefined;
     setContext(user);
   };
-   const isUser = props.user !== undefined;
- 
+  //const isUser = props.user !== undefined;
+
   console.log();
   return (
     <>
       <header>
-        
-        <nav className="container">
-        
-          <div className="row">
-            <div className="col-sm-3">
-              <div className="btno">
-                <Link className="btn btn-lg" to="/">
+        <nav className={styles["container"]}>
+          <div className={styles["row"]}>
+            <div className={styles["col-sm-3"]}>
+              <div className={styles["btno"]}>
+                <Link className={styles["btn"]} to="/">
                   Home
                 </Link>
-                <Link className="btn btn-lg" to="/catalog">
+                <Link className={styles["btn"]} to="/catalog">
                   Catalog
                 </Link>
               </div>
-             
-                <div className="btno">
-                  <Link className="btn btn-lg" to="/login">
+              {!token ? (
+                <div className={styles["btno"]}>
+                  <Link className={styles["btn"]} to="/login">
                     Login
                   </Link>
-                  <Link className="btn btn-lg" to="/register">
+                  <Link className={styles["btn"]} to="/register">
                     Register
                   </Link>
                 </div>
-              
-                <div className="btno">
-                  <Link className="btn btn-lg" to="/create">
+              ) : (
+                <div className={styles["btno"]}>
+                  <Link className={styles["btn"]} to="/create">
                     Orders
                   </Link>
-                  <Link className="btn btn-lg" to="/my-orders">
-                   My Orders
+                  <Link className={styles["btn"]} to="/my-orders">
+                    My Orders
                   </Link>
                   <Link
-                    className="btn btn-lg"
+                    className={styles["btn"]}
                     onClick={logoutHandller}
                     to="/login"
                   >
                     Logout
                   </Link>
                 </div>
-              
+              )}
             </div>
           </div>
         </nav>
-       
       </header>
     </>
   );
