9c954850cb3264510cbfc80072a74025abc75162
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-07-09 18:44:04 +0300

    Update card

--- SPA/src/components/Card/Card.js
+++ SPA/src/components/Card/Card.js
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
-import './Card.css'
+import styles from './Card.module.css'
 //className="btn btn-outline-success btn-sm">
 const Card = (props) => {
   console.log(props,'card');
   return (
-   <div className="wraper">  
-    <div key={props.data._id} className="card">
-      <img src={props.data.imageUrl} alt="..." className="card-img-top" />
-      <div className="card-body">
-        <h5 className="card-title">{props.data.title}</h5>
+   <div className={styles["wraper"]}>  
+    <div key={props.data._id} className={styles["card"]}>
+      <img src={props.data.imageUrl} alt="..." className={styles["card-img-top"]} />
+      <div className={styles["card-body"]}>
+        <h5 className={styles["card-title"]}>{props.data.title}</h5>
        
-        <Link to={`/details/${props.data._id}`} info={props} className="button-top">
+        <Link to={`/details/${props.data._id}`} info={props} className={styles["button-top"]}>
           Read More
         </Link>
         
