84fa271c3b6d1841eee0aec091f9d5045a8ddc88
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-07-10 20:12:56 +0300

    Create new path for Card

---
+++ SPA/src/components/Catalog/Card/Card.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import styles from './Card.module.css'
+//className="btn btn-outline-success btn-sm">
+const Card = (props) => {
+  console.log(props,'card');
+  return (
+   <div className={styles["wraper"]}>  
+    <div key={props.data._id} className={styles["card"]}>
+      <img src={props.data.imageUrl} alt="..." className={styles["card-img-top"]} />
+      <div className={styles["card-body"]}>
+        <h5 className={styles["card-title"]}>{props.data.title}</h5>
+       
+        <Link to={`/details/${props.data._id}`} info={props} className={styles["button-top"]}>
+          Read More
+        </Link>
+        
+      </div>
+    </div>
+    </div> 
+  );
+};
+export default Card;
