9e738e26c9c0ca3b0c717367bfdb193e6feb6d0d
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-07-12 21:41:30 +0300

    Change form to Controlled form

--- SPA/src/components/Create/Create.js
+++ SPA/src/components/Create/Create.js
@@ -1,26 +1,44 @@
 import { useNavigate } from "react-router-dom";
-import AuthContexts from "../../contexts/authContext";
+//import AuthContexts from "../../contexts/authContext";
 import * as requests from "../../services/server";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import styles from "./Create.module.css";
+
 const Create = () => {
   const navigation = useNavigate();
-  const [user, setContext] = useContext(AuthContexts);
-  console.log(user, setContext, "create");
+ // const [user, setContext] = useContext(AuthContexts);
+  const [value, setValue] = useState({
+    title: "",
+    author: "",
+    email: "",
+    address: "",
+    courier: "",
+    number: "",
+    payment: "cash-delivery",
+  });
+  //console.log(user, setContext, "create");
+
+  const changeHendler = (e) => {
+    setValue((state) => ({
+      ...state,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
   const formHandler = async (e) => {
     e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    console.log(formData);
-    const title = formData.get("title").trim();
-    const author = formData.get("author").trim();
-    const email = formData.get("email").trim();
-    const address = formData.get("address").trim();
-    const courier = formData.get("courier").trim();
-    const number = Number(formData.get("number"));
-    const payment = formData.get("payment").trim();
-    const ctx = { title, author, email, address, courier, number, payment };
+    const { title, author, email, address, courier, number, payment } = value;
 
+    const ctx = {
+      title: title.trim(),
+      author: author.trim(),
+      email: email.trim(),
+      address: address.trim(),
+      courier: courier.trim(),
+      number: Number(number),
+      payment,
+    };
+     console.log(payment,'paimentgfhgffg')
     try {
       let token = JSON.parse(localStorage.getItem("user"));
       await requests.createOrder(ctx, token.accessToken);
@@ -50,10 +68,8 @@ const Create = () => {
           ></img>
         </div>
         <form
-          action="#"
           method="POST"
           className={styles["signupForm"]}
-          name="signupform"
           onSubmit={formHandler}
         >
           <ul className={["noBullet"]}>
@@ -64,7 +80,8 @@ const Create = () => {
                 className={styles["inputFields"]}
                 name="title"
                 placeholder="Title: Batman"
-                defaultValue=""
+                value={value.title}
+                onChange={changeHendler}
               />
             </li>
             <li>
@@ -74,7 +91,8 @@ const Create = () => {
                 className={styles["inputFields"]}
                 name="author"
                 placeholder="Author: Bob Kane"
-                defaultValue=""
+                value={value.author}
+                onChange={changeHendler}
               />
             </li>
             <li>
@@ -84,7 +102,8 @@ const Create = () => {
                 className={styles["inputFields"]}
                 name="email"
                 placeholder="Email: ivan@abv.bg"
-                defaultValue=""
+                value={value.email}
+                onChange={changeHendler}
               />
             </li>
             <li>
@@ -94,7 +113,8 @@ const Create = () => {
                 className={styles["inputFields"]}
                 name="address"
                 placeholder="Address: Town and Street "
-                defaultValue=""
+                value={value.address}
+                onChange={changeHendler}
               />
             </li>
             <li>
@@ -104,7 +124,8 @@ const Create = () => {
                 className={styles["inputFields"]}
                 name="courier"
                 placeholder="Courier service: Econt, Speedy "
-                defaultValue=""
+                value={value.courier}
+                onChange={changeHendler}
               />
             </li>
             <li>
@@ -114,11 +135,17 @@ const Create = () => {
                 className={styles["inputFields"]}
                 name="number"
                 placeholder="Number: 1, 2, 3 "
-                defaultValue="1"
+                value={value.number}
+                onChange={changeHendler}
               />
             </li>
 
-            <select name="payment" className={styles["payment"]}>
+            <select
+              name="payment"
+              className={styles["payment"]}
+              value={value.payment}
+              onChange={changeHendler}
+            >
               <option value="cash-delivery">Cash on Delivery</option>
               <option value="credit-card">Credit Card</option>
               <option value="debit-card">Debit Card</option>
@@ -127,9 +154,6 @@ const Create = () => {
               <button
                 className={styles["btn"]}
                 type="submit"
-                name="join"
-                alt=""
-                defaultValue="Submit"
               >
                 Order
               </button>
