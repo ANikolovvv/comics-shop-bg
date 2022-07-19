01bb10a4b64e8ecc833c97c95de43667e51e63cb
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-07-19 22:28:44 +0300

    Update Home.js

--- SPA/src/components/Home/Home.js
+++ SPA/src/components/Home/Home.js
@@ -1,37 +1,34 @@
 import { useEffect, useState } from "react";
 
-import * as requests from "../../services/server";
 import styles from "../Home/Home.module.css";
 import { Spinner } from "../Spinner/Spinner";
 import { Top } from "./TopFive";
-//import { Top } from "./TopFive";
 
-function Home() {
+function Home({ comic }) {
   const [comics, setComics] = useState([]);
   const [count, setCount] = useState(0);
 
   console.log(comics, " top reiting");
 
   useEffect(() => {
-    requests.getAll().then((result) => {
-         if(result !==undefined){
-          const rating=result.sort((a,b)=>b.userLiked.length - a.userLiked.length);
-         
-          setComics(rating);
-         }
-     
-    });
-  }, []);
-  setTimeout(() => {
-    if(count <4){
-     setCount(count+1)
-    }else{
-     setCount(0)
-    }
-   // count is 0 here
- }, 2000);
+    if (comic !== undefined) {
+      const rating = comic.sort(
+        (a, b) => b.userLiked.length - a.userLiked.length
+      );
 
-   console.log('work', count)
+      setComics(rating);
+    }
+  }, [comic]);
+  setTimeout(() => {
+    if (count < 4) {
+      setCount(count + 1);
+    } else {
+      setCount(0);
+    }
+    // count is 0 here
+  }, 2000);
+
+  console.log("work", count);
 
   return (
     <>
@@ -39,10 +36,9 @@ function Home() {
         <h1>Welcome to comics-shop.bg</h1>
         <p>Best Comics of 2022 </p>
       </article>
-       
-      <article >
-        {comics.length>0?
-        <Top info={comics[count]}></Top>:<Spinner/>}
+
+      <article>
+        {comics.length > 0 ? <Top info={comics[count]}></Top> : <Spinner />}
       </article>
     </>
   );
