def58a51a75f680c553647aa8d3a855f6132c3f6
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-07-22 22:07:00 +0300

    Update server

--- SPA/src/services/server.js
+++ SPA/src/services/server.js
@@ -64,15 +64,7 @@ export async function onLogin(data) {
   return res;
 }
 
-// export function getAll() {
 
-//   return fetch(`http://localhost:3030/api`).then((res) => res.json());
-// }
-export async function getAll() {
-  const response = await fetch(`http://localhost:3030/api`);
-
-  return await response.json();
-}
 
 export async function getMyData(id) {
   const response = await fetch(`http://localhost:3030/api/owner/my-data/${id}`);
@@ -103,47 +95,7 @@ export function getOwnerData(id) {
   );
 }
 
-//`http://localhost:3030/api/data/create
-export async function createOrder(data, token) {
-  console.log(token, "creretre");
-  const response = await fetch(`http://localhost:3030/api/data/create`, {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Authorization": token,
-    },
-    body: JSON.stringify(data),
-  });
 
-  return response.json();
-}
-
-export async function updateOrder(data, id, token) {
-  console.log(id, "id");
-  console.log(token, "token");
-  console.log(data, "data");
-  const response = await fetch(`http://localhost:3030/api/owner/${id}`, {
-    method: "put",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Authorization": token,
-    },
-    body: JSON.stringify(data),
-  });
-
-  return await response.json();
-}
-export async function deleteOrder(id, token) {
-  const response = await fetch(`http://localhost:3030/api/owner/${id}`, {
-    method: "delete",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Authorization": token,
-    },
-  });
-
-  return await response.json();
-}
 export async function searchData(data) {
   const response = await fetch(`http://localhost:3030/api/data/search`, {
     method: "post",
