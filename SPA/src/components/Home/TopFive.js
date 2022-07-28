ad9f818ca050f5f4baec3953e192e913539b3e99
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-07-28 21:34:09 +0300

    Update TopFive.js

--- SPA/src/components/Home/TopFive.js
+++ SPA/src/components/Home/TopFive.js
@@ -1,30 +1,27 @@
 //import { useState,useEffect } from "react";
-import styles from  "./TopFive.module.css";
+import styles from "./TopFive.module.css";
 import { Link } from "react-router-dom";
-
 
 export function Top(props) {
   console.log(props.info, "props");
-  const data=props.info;
+  const data = props.info;
 
-    return(
-      <div className={styles["home"]}>
-          <p className={styles["date-title"]}>{data.title}</p>
-        <img
-          src={data.imageUrl}
-          id="image"
-          alt=".."
-        />
-         
-        <div >
-        <Link className={ styles["button-top"]} to={`/details/${data._id}`}>Read more</Link>
-        </div>
-         <p className={styles["date-title"]}>Raiting: {data.userLiked.length}</p>
-        <span>☆</span>
-        <span>☆</span>
-        <span>☆</span>
-        <span>☆</span>
-        <span>☆</span>
+  return (
+    <div className={styles["home"]}>
+      <p className={styles["date-title"]}>{data.title}</p>
+      <img src={data.imageUrl} id="image" alt=".." />
+
+      <div>
+        <Link className={styles["button-top"]} to={`/details/${data._id}`}>
+          Read more
+        </Link>
       </div>
-    )
+      <p className={styles["date-title"]}>Raiting: {data.userLiked.length}</p>
+      <span>☆</span>
+      <span>☆</span>
+      <span>☆</span>
+      <span>☆</span>
+      <span>☆</span>
+    </div>
+  );
 }
