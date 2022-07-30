074356827172154a5c47868f6c4b91be07dc0393
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-07-30 14:10:21 +0300

    Update ownerDataController.js

--- REST_API/controllers/ownerDataController.js
+++ REST_API/controllers/ownerDataController.js
@@ -1,29 +1,23 @@
 const router = require("express").Router();
-const ownerApi=require('../services/userServices')
+const ownerApi = require("../services/userServices");
 const { isAuth, isOwner } = require("../middlewares/isGuard");
 const api = require("../services/orderServices");
 const preload = require("../middlewares/preload");
 
 router.get("/my-data/:id", async (req, res) => {
-  console.log('my data')
-    try {
-       let orders=await ownerApi.getUserData(req.params.id);
-       let history=orders.orderHistory
-       console.log(history,'ghfhfghfghffhf')
-       res.json({history:history})
-    } catch (err) {
-      res.status(400).json({ message: "Request error" });
-    }
-  //let item = await api.getData(req.params.id);
- 
+  try {
+    let orders = await ownerApi.getUserData(req.params.id);
+    let history = orders.orderHistory;
+
+    res.json({ history: history });
+  } catch (err) {
+    res.status(400).json({ message: "Request error" });
+  }
 });
-router.get('/:id',preload(api),async(req,res)=>{
-    console.log('get order')
-    res.json(res.locals.item);
-   
-})
-router.put("/:id", preload(api),isOwner(), async (req, res) => {
-  console.log("editvbb hhh put");
+router.get("/:id", preload(api), async (req, res) => {
+  res.json(res.locals.item);
+});
+router.put("/:id", preload(api), isOwner(), async (req, res) => {
   try {
     const result = await api.updateData(res.locals.item, req.body);
     res.json({ result });
@@ -32,12 +26,9 @@ router.put("/:id", preload(api),isOwner(), async (req, res) => {
     res.status(400).json({ message: err.message });
   }
 });
-router.delete("/:id",preload(api), isAuth(), isOwner(),async (req, res) => {
+router.delete("/:id", preload(api), isAuth(), isOwner(), async (req, res) => {
   const id = req.params.id;
-  console.log("delete ssssssss");
-  // res.json({ok:'ok'})
-  // res.status(200)
- // isAuth(), isOwner(),
+
   try {
     const result = await api.deleteData(id);
     res.json({ result });
