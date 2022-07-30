0f6b9943ed777c0cade2253f6d39150ccd5069b7
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-07-30 14:10:49 +0300

    Update userServices.js

--- REST_API/services/userServices.js
+++ REST_API/services/userServices.js
@@ -2,7 +2,7 @@ const User = require("../models/User");
 
 exports.getUserData = async (id) => {
   const userData = await User.findById(id).populate("orderHistory");
-  console.log(userData,'usererer')
+
   if (!userData) {
     throw new Error("Sory someting when wrong!");
   }
