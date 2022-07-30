5ae620dd332df3094e171bd1d551d586f876b33f
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-07-30 14:11:15 +0300

    Update app js

--- SPA/src/App.js
+++ SPA/src/App.js
@@ -1,61 +1,61 @@
 import { Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 
-import { AuthContexts } from "./contexts/AuthContext";
 import Catalog from "./components/Catalog/Catalog";
 import Create from "./components/Create/Create";
 import Details from "./components/Details/Details";
-import Header from "./components/common/Header/Header";
+import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
-import { Footer } from "./components/common/Footer/Footer";
+
+import { Footer } from "./components/Footer/Footer";
 import { MyOrder } from "./components/MyOrder/MyOrder";
 import { NotFound } from "./components/404/404";
 import { Edit } from "./components/Edit/Edit";
 import Buy from "./components/Buy-create/BuyCreate";
-import { useLocaleStorage } from "./hooks/useLocaleStorage";
+
 import { Contact } from "./components/Contact-us/Contact";
 import useFetch from "./hooks/useFetch";
 import Logout from "./components/Logout/Logout";
+import UserGuard from "./components/common/UserGuard";
+import GuestGuard from "./components/common/GuestGuard";
 
 function App() {
-  const [auth, setAuth] = useLocaleStorage("auth", {});
   const [comics] = useFetch(`http://localhost:3030/api`, []);
 
-  const userLogin = (authData) => {
-    setAuth(authData);
-  };
-
-  const userLogout = () => {
-    setAuth({});
-  };
-
   return (
-    <AuthContexts.Provider value={{ user: auth, userLogin, userLogout }}>
+    <AuthProvider>
       <div className="App">
         <Header></Header>
         <main className="main">
           <Routes>
             <Route path="/" element={<Home comic={comics} />}></Route>
-            <Route path="/about" element={<Contact />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/logout" element={<Logout />}></Route>
-            <Route path="/register" element={<Register />}></Route>
+
             <Route
               path="/catalog"
               element={<Catalog comics={comics} />}
             ></Route>
-            <Route path="/create" element={<Create />}></Route>
-            <Route path="/buy-create/:id" element={<Buy />}></Route>
-            <Route path="/my-orders" element={<MyOrder />}></Route>
+            <Route element={<GuestGuard />}>
+              <Route path="/login" element={<Login />}></Route>
+              <Route path="/register" element={<Register />}></Route>
+            </Route>
+
             <Route path="/details/:id" element={<Details />}></Route>
-            <Route path="/edit/:id" element={<Edit />}></Route>
+            <Route element={<UserGuard />}>
+              <Route path="/logout" element={<Logout />}></Route>
+              <Route path="/create" element={<Create />}></Route>
+              <Route path="/buy-create/:id" element={<Buy />}></Route>
+              <Route path="/my-orders" element={<MyOrder />}></Route>
+              <Route path="/edit/:id" element={<Edit />}></Route>
+            </Route>
             <Route path="*" element={<NotFound />}></Route>
+            <Route path="/about" element={<Contact />}></Route>
           </Routes>
         </main>
         <Footer />
       </div>
-    </AuthContexts.Provider>
+    </AuthProvider>
   );
 }
 
