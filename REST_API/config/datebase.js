a73af4cf5cfc604c947f8f96186c0a8adef58bbf
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-07-31 20:46:13 +0300

    Update datebase

--- REST_API/config/datebase.js
+++ REST_API/config/datebase.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 async function db() {
 
   try {
-    await mongoose.connect(process.env.DATABASE_URL||process.env.MONGODB_URL|| process.env.EXPRESS_PORT,{useNewUrlParser: true, dbName: "comics-shop"});
+    await mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true, dbName: "comics-shop"});
     console.log("DB is working :)");
   } catch (err) {
     console.log(err, "Error in Datebase !");
