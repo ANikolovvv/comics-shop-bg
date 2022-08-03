c751ad8de7da0ad8f491cf98900cf26646028d4a
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-08-03 21:20:58 +0300

    Update Contact as

--- SPA/src/components/Contact-us/Contact.js
+++ SPA/src/components/Contact-us/Contact.js
@@ -1,11 +1,13 @@
 import styles from "./Contact.module.css";
-
+import { Map } from "./Map/Map";
 export const Contact = () => {
   return (
     <>
+
       <article className={styles["art"]}>
         <h1>Contact us</h1>
       </article>
+      
       <div className={styles["signupSection"]}>
         <div className={styles["info-form"]}>
           <div className={styles["about"]}>
@@ -17,7 +19,7 @@ export const Contact = () => {
             <li className={styles["li"]}>Comics-shop email :</li>
             <li className={styles["li-info"]}> comicsshop@red.bg</li>
             <li className={styles["li"]}>Comics-shop address :</li>
-            <li className={styles["li-info"]}>Burgas str. Dark House 5 </li>
+            <li className={styles["li-info"]}>Burgas str. Oborishte </li>
           </ul>
         </div>
         <img
@@ -25,7 +27,9 @@ export const Contact = () => {
           src="https://starwarsblog.starwars.com/wp-content/uploads/2020/12/marvel-star-wars-12-cover.jpg"
           alt="..."
         ></img>
+        
       </div>
+      <Map></Map>
     </>
   );
 };
