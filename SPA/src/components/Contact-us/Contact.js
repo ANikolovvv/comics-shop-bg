535cc5a7acb5a7c682690738ae53afc74b6d0714
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-11 19:32:16 +0200

    Update Contact.js

--- SPA/src/components/Contact-us/Contact.js
+++ SPA/src/components/Contact-us/Contact.js
@@ -1,37 +1,40 @@
-import { MapProvider } from "react-map-gl";
 import styles from "./Contact.module.css";
 import { Map } from "./Map/Map";
 
 export const Contact = () => {
   return (
     <>
-      <article className={styles["art"]}>
-        <h1>Contact us</h1>
-      </article>
+      <div className={styles["container"]}>
+        <article className={styles["art"]}>
+          <h1>Contact us</h1>
+        </article>
+        <article className={styles["contact"]}>
+          <div className={styles["contactSection"]}>
+            <div className={styles["info-form"]}>
+              <h2 className={styles["about"]}>
+                If you have any question you can contact us.
+              </h2>
+              <ul>
+                <li className={styles["li"]}>Comics-shop number :</li>
+                <li className={styles["li-info"]}>0887776667</li>
+                <li className={styles["li"]}>Comics-shop email :</li>
+                <li className={styles["li-info"]}> comicsshop@red.bg</li>
+                <li className={styles["li"]}>Comics-shop address :</li>
+                <li className={styles["li-info"]}>Burgas str. Oborishte </li>
+              </ul>
+            </div>
+            <div className={styles["div-icon"]}>
+              <img
+                className={styles["icon"]}
+                src="https://starwarsblog.starwars.com/wp-content/uploads/2020/12/marvel-star-wars-12-cover.jpg"
+                alt="..."
+              ></img>
+            </div>
 
-      <div className={styles["signupSection"]}>
-        <div className={styles["info-form"]}>
-          <div className={styles["about"]}>
-            If you have any question you can contact us.
+            <Map></Map>
           </div>
-          <ul>
-            <li className={styles["li"]}>Comics-shop number :</li>
-            <li className={styles["li-info"]}>0887776667</li>
-            <li className={styles["li"]}>Comics-shop email :</li>
-            <li className={styles["li-info"]}> comicsshop@red.bg</li>
-            <li className={styles["li"]}>Comics-shop address :</li>
-            <li className={styles["li-info"]}>Burgas str. Oborishte </li>
-          </ul>
-        </div>
-        <img
-          className={styles["icon"]}
-          src="https://starwarsblog.starwars.com/wp-content/uploads/2020/12/marvel-star-wars-12-cover.jpg"
-          alt="..."
-        ></img>
+        </article>
       </div>
-      <MapProvider>
-        <Map></Map>
-      </MapProvider>
     </>
   );
 };
