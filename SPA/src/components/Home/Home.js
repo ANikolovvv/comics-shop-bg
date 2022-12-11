18a4ef008035489304a01ed86875c8cb14605ce4
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-11 19:32:27 +0200

    Update Home.js

--- SPA/src/components/Home/Home.js
+++ SPA/src/components/Home/Home.js
@@ -32,17 +32,20 @@ function Home({ comic }) {
         <h1>Welcome to comics-shop.bg</h1>
         <p>Best Comics of 2022 </p>
       </article>
-
-      <article>
-        {comics.length > 0 ? <Top info={comics[count]}></Top> : <Spinner />}
-      </article>
-      <div className={styles["home-art"]}>
-        <div className={styles["art-info"]}>
-          comics-shop.bg is an app for buying online comics.Inside the app you
-          can find different comics from different countries, and you can order
-          a comics which we don't have in the moment.You may cancel and update
-          an online order for any reason at any time up to the end of the fifth
-          working day, after you place your order.
+      <div className={styles["container"]}>
+        <article className={styles["info"]}>
+          {comics.length > 0 ? <Top info={comics[count]}></Top> : <Spinner />}
+        </article>
+        <div className={styles["home-art"]}>
+          <div className={styles["art-info"]}>
+            <p>
+              comics-shop.bg is an app for buying online comics.Inside the app
+              you can find different comics from different countries, and you
+              can order a comics which we don't have in the moment.You may
+              cancel and update an online order for any reason at any time up to
+              the end of the fifth working day, after you place your order.
+            </p>
+          </div>
         </div>
       </div>
     </>
