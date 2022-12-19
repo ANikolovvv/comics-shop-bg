98918c40bee5f6de04f867bc68560e242111fde4
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-19 21:01:08 +0200

    Update MyCard.js

--- SPA/src/components/MyOrder/MyCard/MyCard.js
+++ SPA/src/components/MyOrder/MyCard/MyCard.js
@@ -5,21 +5,24 @@ export const MyCard = (props) => {
   return (
     <div className={styles["wraper"]}>
       <div key={props.data._id} className={styles["card"]}>
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/91UH4LoDTvL.jpg"
-          alt="..."
-          className={styles["card-img-top"]}
-        />
+        <div className={styles["img"]}>
+          <img
+            src="https://images-na.ssl-images-amazon.com/images/I/91UH4LoDTvL.jpg"
+            alt="..."
+            className={styles["card-img-top"]}
+          />
+        </div>
         <div className={styles["card-body"]}>
           <h5 className={styles["card-title"]}>Title: {props.data.title}</h5>
           <p className={styles["card-title"]}>Price: {props.data.price} </p>
-
-          <Link to={`/edit/${props.data._id}`} className={styles["btn"]}>
-            Edit
-          </Link>
-          <button className={styles["btn-delete"]} onClick={props.onClick}>
-            Delete
-          </button>
+          <div className={styles["buttons"]}>
+            <Link to={`/edit/${props.data._id}`} className={styles["btn"]}>
+              Edit
+            </Link>
+            <button className={styles["btn"]} onClick={props.onClick}>
+              Delete
+            </button>
+          </div>
         </div>
       </div>
     </div>
