0c20aac9f092de77e31dd7b3da4a92481b21236b
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-20 21:25:48 +0200

    Update Create.js

--- SPA/src/components/Create/Create.js
+++ SPA/src/components/Create/Create.js
@@ -98,7 +98,9 @@ const Create = () => {
         >
           <ul className={["noBullet"]}>
             <li>
-              <label htmlFor="text"></label>
+              <label htmlFor="text">
+                <i class="fa-sharp fa-solid fa-t"></i> Ttle
+              </label>
               <input
                 type="text"
                 className={styles["inputFields"]}
@@ -108,14 +110,17 @@ const Create = () => {
                 onChange={changeHendler}
                 onBlur={(e) => minLength(e, 3)}
               />
+              {errors.title && (
+                <p className={styles["error-form"]}>
+                  Title should be at least 3 characters long!
+                </p>
+              )}
             </li>
-            {errors.title && (
-              <p className={styles["error-form"]}>
-                Title should be at least 3 characters long!
-              </p>
-            )}
+
             <li>
-              <label htmlFor="text"></label>
+              <label htmlFor="text">
+                <i class="fa-solid fa-font"></i> Author
+              </label>
               <input
                 type="text"
                 className={styles["inputFields"]}
@@ -125,14 +130,17 @@ const Create = () => {
                 onChange={changeHendler}
                 onBlur={(e) => minLength(e, 3)}
               />
+              {errors.author && (
+                <p className={styles["error-form"]}>
+                  Author should be at least 3 characters long!
+                </p>
+              )}
             </li>
-            {errors.author && (
-              <p className={styles["error-form"]}>
-                Author should be at least 3 characters long!
-              </p>
-            )}
+
             <li>
-              <label htmlFor="email"></label>
+              <label htmlFor="email">
+                <i class="fa-solid fa-envelope"></i> Email
+              </label>
               <input
                 type="email"
                 className={styles["inputFields"]}
@@ -142,14 +150,17 @@ const Create = () => {
                 onChange={changeHendler}
                 onBlur={(e) => minLength(e, 8)}
               />
+              {errors.email && (
+                <p className={styles["error-form"]}>
+                  Email is not valid - valid email red@abv.bg!
+                </p>
+              )}
             </li>
-            {errors.email && (
-              <p className={styles["error-form"]}>
-                Email is not valid - valid email red@abv.bg!
-              </p>
-            )}
+
             <li>
-              <label htmlFor="text"></label>
+              <label htmlFor="text">
+                <i class="fa-solid fa-font"></i> Address
+              </label>
               <input
                 type="text"
                 className={styles["inputFields"]}
@@ -159,14 +170,17 @@ const Create = () => {
                 onChange={changeHendler}
                 onBlur={(e) => minLength(e, 10)}
               />
+              {errors.address && (
+                <p className={styles["error-form"]}>
+                  Address should be at least 10 characters long!
+                </p>
+              )}
             </li>
-            {errors.address && (
-              <p className={styles["error-form"]}>
-                Address should be at least 10 characters long!
-              </p>
-            )}
+
             <li>
-              <label htmlFor="text"></label>
+              <label htmlFor="text">
+                <i class="fa-solid fa-truck"></i> Courier
+              </label>
               <input
                 type="text"
                 className={styles["inputFields"]}
@@ -176,14 +190,17 @@ const Create = () => {
                 onChange={changeHendler}
                 onBlur={(e) => minLength(e, 4)}
               />
+              {errors.courier && (
+                <p className={styles["error-form"]}>
+                  Courier should Econt or Speedy!
+                </p>
+              )}
             </li>
-            {errors.courier && (
-              <p className={styles["error-form"]}>
-                Courier should Econt or Speedy!
-              </p>
-            )}
+
             <li>
-              <label htmlFor="number"></label>
+              <label htmlFor="number">
+                <i class="fa-solid fa-arrow-up-1-9"></i> Number
+              </label>
               <input
                 type="number"
                 className={styles["inputFields"]}
@@ -193,12 +210,13 @@ const Create = () => {
                 onChange={changeHendler}
                 onBlur={isNumber}
               />
+              {errors.number && (
+                <p className={styles["error-form"]}>
+                  Number should be biger then 0!
+                </p>
+              )}
             </li>
-            {errors.number && (
-              <p className={styles["error-form"]}>
-                Number should be biger then 0!
-              </p>
-            )}
+
             <select
               name="payment"
               className={styles["payment"]}
