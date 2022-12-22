5742b85533f255d1a33525dbb840cd9887df8bf8
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-22 15:42:45 +0200

    Update Contact.js

--- SPA/src/components/Contact-us/Contact.js
+++ SPA/src/components/Contact-us/Contact.js
@@ -14,12 +14,12 @@ export const Contact = () => {
               <h2 className={styles["about"]}>
                 If you have any question you can contact us.
               </h2>
-              <ul>
-                <li className={styles["li"]}>Comics-shop number :</li>
-                <li className={styles["li-info"]}>0887776667</li>
-                <li className={styles["li"]}>Comics-shop email :</li>
+              <ul className={styles["ul"]}>
+                <li className={styles["li"]}>Comics-shop number:</li>
+                <li className={styles["li-info"]}>088 777 6667</li>
+                <li className={styles["li"]}>Comics-shop email:</li>
                 <li className={styles["li-info"]}> comicsshop@red.bg</li>
-                <li className={styles["li"]}>Comics-shop address :</li>
+                <li className={styles["li"]}>Comics-shop address:</li>
                 <li className={styles["li-info"]}>Burgas str. Oborishte </li>
               </ul>
             </div>
