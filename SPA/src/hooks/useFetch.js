1f110a2a2dcf0bab6b30fa2d3557ba7c9325ada1
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-22 19:49:16 +0200

    Update useFetch.js

--- SPA/src/hooks/useFetch.js
+++ SPA/src/hooks/useFetch.js
@@ -1,20 +1,23 @@
 import { useEffect, useState } from "react";
 
 const useFetch = (defautValue) => {
-  const url =  'https://comics-shop-bg.onrender.com/api' ||`http://localhost:3030/api` ;
+  const url =
+    "https://comics-shop-bg.onrender.com/api";
   const [data, setData] = useState(defautValue);
 
   useEffect(() => {
-    setTimeout(()=>{
-      fetch(url)
-      .then((res) => res.json())
-      .then((result) => {
-        if (result !== undefined) {
-          setData(result);
-        }
-      });
-    },1000)
-    
+    // fetch(url)
+    //   .then((res) => res.json())
+    //   .then((result) => {
+    //     if (result !== undefined) {
+    //       setData(result);
+    //     }
+    //   });
+     const dataFetch=async()=>{
+      const data=await(await fetch(url)).json();
+      setData(data)
+     };
+     dataFetch();
   }, [url]);
   return [data];
 };
