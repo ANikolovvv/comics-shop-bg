ff7826a5f73962dcbe268df21d461d621d40a0cb
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-01-07 14:03:03 +0200

    Update homeController.js

--- REST_API/controllers/homeController.js
+++ REST_API/controllers/homeController.js
@@ -5,7 +5,7 @@ const comicServices = require("../services/comicsServices");
 router.get("/", async (req, res) => {
   try {
     let data = await comicServices.getAllComics();
-
+    req.setTimeout(1000);
     res.json(data);
   } catch (error) {
     res.status(400).json({ message: "Bad request" });
