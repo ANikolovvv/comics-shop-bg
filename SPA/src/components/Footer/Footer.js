a249b8366ff78f4affb1218825e1acb1874a8dd2
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-08-12 18:58:53 +0300

    Update Footer.js

--- SPA/src/components/Footer/Footer.js
+++ SPA/src/components/Footer/Footer.js
@@ -1,4 +1,6 @@
 import styles from "./Footer.module.css";
-export const Footer = () => {
+
+ const Footer = () => {
   return <footer className={styles["footer"]}> &copy; Comics-Shop 2022</footer>;
 };
+export default Footer
