c6838251f6f4adfcc811129f599f9556f9136675
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-08-12 19:00:12 +0300

    Create images.js

---
+++ SPA/src/helpers/images.js
@@ -0,0 +1,5 @@
+export const images = [
+  "https://3.bp.blogspot.com/-Yy1u6V_K1GM/ViVxBmGYORI/AAAAAAARXVw/RQ_Pv-BtTkA/s1600/3_00.jpg",
+  "https://i.ebayimg.com/images/g/B8IAAOSwgSthDdgU/s-l1600.jpg",
+  "https://i.pinimg.com/236x/9f/fa/6e/9ffa6ed06be1f65082360d42461c86b7.jpg",
+];
