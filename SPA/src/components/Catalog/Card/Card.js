50c5483e6d20447a2975b1a1e921ddd000f1e001
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-08-13 16:44:48 +0300

    Update Card.js

--- SPA/src/components/Catalog/Card/Card.js
+++ SPA/src/components/Catalog/Card/Card.js
@@ -1,26 +1,16 @@
-import { Link } from "react-router-dom";
 import styles from "./Card.module.css";
+import Button from "../../UI/Button/Button";
 
 const Card = (props) => {
   return (
-    <div className={styles["wraper"]}>
-      <div key={props.data._id} className={styles["card"]}>
-        <div className={styles["box-image"]}>
-          <img
-            src={props.data.imageUrl}
-            alt="comics"
-            className={styles["card-img-top"]}
-          />
-        </div>
-        <div className={styles["card-body"]}>
-          <h5 className={styles["card-title"]}>{props.data.title}</h5>
-          <Link
-            to={`/details/${props.data._id}`}
-            info={props}
-            className={styles["button-top"]}
-          >
-            Read More
-          </Link>
+    <div key={props.data._id} className={styles["card"]}>
+      <div className={styles["box-image"]}>
+        <img src={props.data.imageUrl} alt="...comics" />
+      </div>
+      <div className={styles["card-body"]}>
+        <h2 className={styles["card-title"]}>{props.data.title}</h2>
+        <div>
+          <Button to={`/details/${props.data._id}`}>Read More</Button>
         </div>
       </div>
     </div>
