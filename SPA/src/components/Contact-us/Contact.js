d94e3a89e7e02306fda5e7106fd2dba5fca92b20
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-08-19 13:08:41 +0300

    Update Contact.js

--- SPA/src/components/Contact-us/Contact.js
+++ SPA/src/components/Contact-us/Contact.js
@@ -6,47 +6,42 @@ import { BsInstagram } from "react-icons/bs";
 
 const Contact = () => {
   return (
-    <>
-      <div className={styles["container"]}>
-        <article className={styles["art"]}>
-          <h1>Contact us</h1>
-        </article>
-        <article className={styles["contact"]}>
-          <div className={styles["contactSection"]}>
-            <div className={styles["info-form"]}>
-              <ul className={styles["ul"]}>
-                <li className={styles["li"]}>Comics-shop number:</li>
-                <li className={styles["li"]}>088 777 6667</li>
-                <li className={styles["li"]}>Comics-shop email:</li>
-                <li className={styles["li"]}> comicsshop@red.bg</li>
-                <li className={styles["li"]}>Comics-shop address:</li>
-                <li className={styles["li"]}>Burgas str. Oborishte </li>
-              </ul>
-              <div className={styles["react-icons"]}>
-                <div className={styles["react-icon"]}>
-                  <FaFacebookF />
-                </div>
-                <div className={styles["react-icon"]}>
-                  <FaTwitter />
-                </div>
-                <div className={styles["react-icon"]}>
-                  <BsInstagram />
-                </div>
+    <div className={styles["container"]}>
+      <div>
+        <h1>Contact us</h1>
+      </div>
+
+      <div className={styles["contact"]}>
+        <div className={styles["contactSection"]}>
+          <div className={styles["info-form"]}>
+            <ul className={styles["ul"]}>
+              <li className={styles["li"]}>
+                Comics-shop number:<span>088 777 6667</span>
+              </li>
+              <li className={styles["li"]}>
+                Comics-shop email: <span>comicsshop@red.bg</span>
+              </li>
+              <li className={styles["li"]}>
+                Comics-shop address: <span>Burgas str. Oborishte</span>
+              </li>
+            </ul>
+            <div className={styles["react-icons"]}>
+              <div className={styles["react-icon"]}>
+                <FaFacebookF />
+              </div>
+              <div className={styles["react-icon"]}>
+                <FaTwitter />
+              </div>
+              <div className={styles["react-icon"]}>
+                <BsInstagram />
               </div>
             </div>
-            <div className={styles["div-icon"]}>
-              <img
-                className={styles["icon"]}
-                src="https://starwarsblog.starwars.com/wp-content/uploads/2020/12/marvel-star-wars-12-cover.jpg"
-                alt="..."
-              ></img>
-            </div>
-
-            <Map></Map>
           </div>
-        </article>
+
+          <Map />
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 export default Contact;
