d819c773fc9afa7d25776ad2ac2012fe658c3f74
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-08-26 12:58:25 +0300

    Update MyCard.js

--- SPA/src/components/MyOrder/MyCard/MyCard.js
+++ SPA/src/components/MyOrder/MyCard/MyCard.js
@@ -3,26 +3,24 @@ import { Link } from "react-router-dom";
 
 export const MyCard = (props) => {
   return (
-    <div className={styles["wraper"]}>
-      <div key={props.data._id} className={styles["card"]}>
-        <div className={styles["img"]}>
-          <img
-            src="https://images-na.ssl-images-amazon.com/images/I/91UH4LoDTvL.jpg"
-            alt="..."
-            className={styles["card-img-top"]}
-          />
-        </div>
-        <div className={styles["card-body"]}>
-          <h5 className={styles["card-title"]}>Title: {props.data.title}</h5>
-          <p className={styles["card-title"]}>Price: {props.data.price} </p>
-          <div className={styles["buttons"]}>
-            <Link to={`/edit/${props.data._id}`} className={styles["btn"]}>
-              Edit
-            </Link>
-            <button className={styles["btn"]} onClick={props.onClick}>
-              Delete
-            </button>
-          </div>
+    <div key={props.data._id} className={styles["card"]}>
+      <div className={styles["img"]}>
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/I/91UH4LoDTvL.jpg"
+          alt="..."
+          className={styles["card-img-top"]}
+        />
+      </div>
+      <div className={styles["card-body"]}>
+        <h5 className={styles["card-title"]}>{props.data.title}</h5>
+        <p >Price: {props.data.price}$ </p>
+        <div className={styles["buttons"]}>
+          <Link to={`/edit/${props.data._id}`} className={styles["btn"]}>
+            Edit
+          </Link>
+          <button className={styles["btn"]} onClick={props.onClick}>
+            Delete
+          </button>
         </div>
       </div>
     </div>
