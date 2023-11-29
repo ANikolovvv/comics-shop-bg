3b7f39c41271bbf6e7de9851f274f9601858f0f1
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-11-29 17:35:57 +0200

    Update App.js

--- SPA/src/App.js
+++ SPA/src/App.js
@@ -1,27 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./contexts/authContext";
 
+import Create from "./components/Create";
+import Details from "./components/Details/Details";
+import Home from "./components/Home/Home";
+
+import Footer from "./components/Footer";
+import NotFound from "./components/404/404";
+import Edit from "./components/Edit";
+
+import Contact from "./components/Contact-us/Contact";
+import useFetch from "./hooks/useFetch";
+
+import MyOrder from "./components/MyOrder/MyOrder";
+import Login from "./components/users/Login";
+import Register from "./components/users/Register";
 
 import Catalog from "./components/Catalog/Catalog";
-import Create from "./components/Create/Create";
-import Details from "./components/Details/Details";
-
-import Header from "./components/Header/Header";
-import Home from "./components/Home/Home";
-import Login from "./components/Login/Login";
-import Register from "./components/Register/Register";
-
-import Footer from "./components/Footer/Footer";
-import NotFound from "./components/404/404";
-import  Edit  from "./components/Edit/Edit";
-
-import  Contact  from "./components/Contact-us/Contact";
-import useFetch from "./hooks/useFetch";
-import Logout from "./components/Logout/Logout";
-
-import UserGuard from "./components/common/UserGuard";
-import GuestGuard from "./components/common/GuestGuard";
-import MyOrder from "./components/MyOrder/MyOrder";
+import { GuestGuard, UserGuard } from "./common/authGuard.js";
+import Header from "./components/Header/Header.js";
 
 
 function App() {
@@ -29,41 +26,35 @@ function App() {
 
   return (
     <AuthProvider>
-      <div className="App">
-        <Header></Header>
-        <main className="main">
-          <Routes>
-            <Route path="/" element={<Home comic={comics} />}></Route>
+      <Header/>
+      <main className="main">
+        <Routes>
+          <Route path="/" element={<Home comic={comics} />}></Route>
 
+          <Route path="/catalog" element={<Catalog comics={comics} />}></Route>
+          <Route element={<GuestGuard />}>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/register" element={<Register />}></Route>
+          </Route>
+
+          <Route path="/details/:id" element={<Details />}></Route>
+          <Route element={<UserGuard/>}>
+            <Route path="/create" element={<Create />}></Route>
             <Route
-              path="/catalog"
-              element={<Catalog comics={comics} />}
+              path="/buy-create/:id"
+              element={<Edit title={"Order"} name={"buy"} />}
             ></Route>
-            <Route element={<GuestGuard />}>
-              <Route path="/login" element={<Login />}></Route>
-              <Route path="/register" element={<Register />}></Route>
-            </Route>
-
-            <Route path="/details/:id" element={<Details />}></Route>
-            <Route element={<UserGuard />}>
-              <Route path="/logout" element={<Logout />}></Route>
-              <Route path="/create" element={<Create />}></Route>
-              <Route
-                path="/buy-create/:id"
-                element={<Edit title={"Order"} name={'buy'} />}
-              ></Route>
-              <Route path="/my-orders/" element={<MyOrder />}></Route>
-              <Route
-                path="/edit/:id"
-                element={<Edit title={"Change order"} name={'edit'} />}
-              ></Route>
-            </Route>
-            <Route path="*" element={<NotFound />}></Route>
-            <Route path="/about" element={<Contact />}></Route>
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+            <Route path="/my-orders/" element={<MyOrder />}></Route>
+            <Route
+              path="/edit/:id"
+              element={<Edit title={"Change order"} name={"edit"} />}
+            ></Route>
+          </Route>
+          <Route path="*" element={<NotFound />}></Route>
+          <Route path="/about" element={<Contact />}></Route>
+        </Routes>
+      </main>
+      <Footer />
     </AuthProvider>
   );
 }
