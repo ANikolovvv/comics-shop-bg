b1c798788b6ba7bff4b9b39d8b37ea3d718128b0
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-11-29 17:36:51 +0200

    Update Header.js

--- SPA/src/components/Header/Header.js
+++ SPA/src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 import { AuthContexts } from "../../contexts/authContext";
@@ -6,91 +6,99 @@ import { useContext } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 
 import logo from "../../logo.svg";
-import styles from "./Header.module.css";
+import styles from "./Header.module.scss";
 import Navigation from "./Navigation";
+import { resLogout } from "../../services/server";
 
 const Header = () => {
-  const { user } = useContext(AuthContexts);
+  const { user, userLogout } = useContext(AuthContexts);
   const [toggle, setToggle] = useState(false);
+  const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    resLogout(user.accessToken)
+      .then(() => {
+        userLogout();
+        navigate("/login");
+      })
+      .catch(() => {
+        navigate("/");
+      });
+  };
 
   const navigationHandler = () => {
     setToggle(!toggle);
-    console.log(toggle);
   };
 
   return (
-    <>
-      <header className={styles["header"]}>
-        <nav className={styles["container"]}>
-          <div className={styles["logo"]}>
-            <img src={logo} className={styles["logo-img"]} alt="LOGO"></img>
-          </div>
-        
-          <div className={styles["nav-small"]} onClick={navigationHandler}>
-            {toggle ? (
-              <div className={styles["small"]}>
-                <Navigation user={user}></Navigation>
-              </div>
-            ) : (
-              <div className={styles["small"]}>
-                <AiOutlineMenu size={25} color="#fff" />
-              </div>
-            )}
-          </div>
-          <div className={styles["nav-box"]}>
-            <ul className={styles["box__ul"]}>
-              <li>
-                <Link className={styles["link"]} to="/">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link className={styles["link"]} to="/catalog">
-                Catalog
-                </Link>
-              </li>
-              <li>
-                <Link className={styles["link"]} to="/about">
-                  Contact us
-                </Link>
-              </li>
-            </ul>
-            {!user.email ? (
-              <ul className={styles["box__ul"]}>
-                <li>
-                  <Link className={styles["link"]} to="/login">
-                    Login
-                  </Link>
-                </li>
-                <li>
-                  <Link className={styles["link"]} to="/register">
-                    Register
-                  </Link>
-                </li>
-              </ul>
-            ) : (
-              <ul className={styles["box__ul"]}>
-                <li>
-                  <Link className={styles["link"]} to="/create">
-                    Orders
-                  </Link>
-                </li>
-                <li>
-                  <Link className={styles["link"]} to="/my-orders">
-                    My Orders
-                  </Link>
-                </li>
-                <li>
-                  <Link className={styles["link"]} to="/logout">
-                    Logout
-                  </Link>
-                </li>
-              </ul>
-            )}
-          </div>
-        </nav>
-      </header>
-    </>
+    <header className={styles["header"]}>
+      <nav className={styles["container"]}>
+        <div className={styles["logo"]}>
+          <img src={logo} className={styles["logo__img"]} alt="LOGO"></img>
+        </div>
+
+        <div className={styles["nav__small"]} onClick={navigationHandler}>
+          {toggle ? (
+            <div className={styles["small"]}>
+              <Navigation user={user}/>
+            </div>
+          ) : (
+            <div className={styles["small"]}>
+              <AiOutlineMenu size={25} color="#fff" />
+            </div>
+          )}
+        </div>
+        <ul className={styles["box__ul"]}>
+          <li className={styles["box__link"]}>
+            <NavLink  to="/">
+              Home
+            </NavLink>
+          </li>
+          <li className={styles["box__link"]}>
+            <NavLink  to="/catalog">
+              Catalog
+            </NavLink>
+          </li>
+          <li className={styles["box__link"]}>
+            <NavLink  to="/about">
+              Contact us
+            </NavLink>
+          </li>
+        </ul>
+        {!user.email ? (
+          <ul className={styles["box__ul"]}>
+            <li className={styles["box__link"]}>
+              <NavLink  to="/login">
+                Login
+              </NavLink>
+            </li>
+            <li className={styles["box__link"]}>
+              <NavLink to="/register">
+                Register
+              </NavLink>
+            </li>
+          </ul>
+        ) : (
+          <ul className={styles["box__ul"]}>
+            <li >
+              <NavLink  to="/create">
+                Orders
+              </NavLink>
+            </li>
+            <li className={styles["box__link"]} >
+              <NavLink  to="/my-orders">
+                My Orders
+              </NavLink>
+            </li>
+            <li className={styles["box__link"]}>
+              <button onClick={logoutHandler}>
+                Logout
+              </button>
+            </li>
+          </ul>
+        )}
+      </nav>
+    </header>
   );
 };
 export default Header;
