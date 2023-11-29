932aafaeb56443980de853f8a3c1382189c862f4
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-11-29 17:36:20 +0200

    Card

---
+++ SPA/src/components/Catalog/Card.js
@@ -0,0 +1,19 @@
+import styles from "./Card.module.css";
+import Button from "../UI/Button/Button";
+
+const Card = (props) => {
+  return (
+    <div key={props.data._id} className={styles["card"]}>
+      <div className={styles["box-image"]}>
+        <img src={props.data.imageUrl} alt="...comics" />
+      </div>
+      <div className={styles["card-body"]}>
+        <h2 className={styles["card-title"]}>{props.data.title}</h2>
+        <div>
+          <Button to={`/details/${props.data._id}`}>Read More</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Card;
