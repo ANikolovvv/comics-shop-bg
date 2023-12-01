a6e9f946f8596d44b617b159b7a84c281234940f
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-12-01 16:01:01 +0200

    Update Home.js

--- SPA/src/components/Home/Home.js
+++ SPA/src/components/Home/Home.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 
 import styles from "./home.module.scss";
-import { Top } from "./topFive";
 import Spinner from "../spinner";
+import TopFive from "./topFive";
 
 function Home({ comic }) {
   const [comics, setComics] = useState([]);
@@ -30,22 +30,19 @@ function Home({ comic }) {
         Discover the Best Comics of 2023 at comics-shop.bg
       </h1>
       {comics.length === 0 && <Spinner />}
-      <div className={styles["container"]}>
-          {comics.length > 0 && <Top info={comics[count]} />}
-        <div className={styles["left-page"]}>
-          <div className={styles["art-info"]}>
-            <i className="fa-solid fa-book"> comics-shop.bg</i>
-            <p>
-              "Discover the world of comics with comics-shop.bg – your gateway
-              to a diverse collection of online comic books. Immerse yourself in
-              a treasure trove of captivating narratives from various countries.
-              If you stumble upon a comic that's not currently available, worry
-              not – you have the power to request it. We offer the flexibility
-              to modify or cancel your online order anytime within the first
-              five business days after placing it, ensuring your satisfaction is
-              our priority."
-            </p>
-          </div>
+      {comics.length > 0 && <TopFive info={comics[count]} />}
+      <div className={styles["left-page"]}>
+        <div className={styles["art-info"]}>
+          <i className="fa-solid fa-book"> comics-shop.bg</i>
+          <p>
+            "Discover the world of comics with comics-shop.bg – your gateway to
+            a diverse collection of online comic books. Immerse yourself in a
+            treasure trove of captivating narratives from various countries. If
+            you stumble upon a comic that's not currently available, worry not –
+            you have the power to request it. We offer the flexibility to modify
+            or cancel your online order anytime within the first five business
+            days after placing it, ensuring your satisfaction is our priority."
+          </p>
         </div>
       </div>
     </div>
