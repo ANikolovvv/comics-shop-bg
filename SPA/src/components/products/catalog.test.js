59b82b2660ac8c821170b600aa2be74d814e9c44
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-12-01 16:34:04 +0200

    Create Catalog component

---
+++ SPA/src/components/products/catalog.test.js
@@ -0,0 +1,40 @@
+import { BrowserRouter } from "react-router-dom";
+import { cleanup, render, screen } from "@testing-library/react";
+import Catalog from "./catalog";
+
+import { AuthProvider } from "../../contexts/authContext";
+
+test("Loading comics from server", async () => {
+  const data = await fetch("https://test-server-rest-api.herokuapp.com/api/");
+  const res = await data.json();
+  function wait(time) {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(res);
+      }, time);
+    });
+
+    return promise;
+  }
+  const all = await wait(3000);
+
+  expect(all.length).not.toBe(0);
+});
+test("render Title and Spinner", async () => {
+  const { getByText } = render(
+    <BrowserRouter>
+      <AuthProvider
+        value={{
+          user: { email: "dsasAbc." },
+        }}
+      >
+        <Catalog></Catalog>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+
+  const title = getByText("Catalog");
+  const spiner = getByText("Loading...");
+  expect(title).toBeInTheDocument();
+  expect(spiner).toBeInTheDocument();
+});
