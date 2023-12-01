3b78ec49e6b42c8680fc026fbc481bf7e0972b0a
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-12-01 18:46:18 +0200

    Optimize TopFive component and SCSS

--- SPA/src/components/home/topFive.js
+++ SPA/src/components/home/topFive.js
@@ -1,36 +1,27 @@
-import Button from "../../elements/button";
+
 import styles from "./topFive.module.scss";
+import { routes } from "../../helpers/routeConfig";
+import { Link } from "react-router-dom";
 
 function TopFive(props) {
   const data = props.info;
+  const path=routes.details.replace("/:id", "/");
 
   return (
-    <div className={styles["home"]}>
-      <h2 className={styles["title"]}>{data.title}</h2>
-      <div>
+    <div className={styles["box__top"]}>
+      <div className={styles["box__img"]}>
         <img src={data.imageUrl} id="image" alt=".." />
       </div>
 
-      <div>
-        <Button to={`/details/${data._id}`}>Read more</Button>
+      <div className={styles["box__info"]}>
+      <h2 className={styles["title"]}>{data.title}</h2>
+        <Link
+          className={styles["link"]}
+          to={`${path}${data._id}`}
+        >
+          Read more
+        </Link>
       </div>
-      <ul className={styles["star"]}>
-        <li>
-          <i className="fa-solid fa-star"></i>
-        </li>
-        <li>
-          <i className="fa-solid fa-star"></i>
-        </li>
-        <li>
-          <i className="fa-solid fa-star"></i>
-        </li>
-        <li>
-          <i className="fa-solid fa-star"></i>
-        </li>
-        <li>
-          <i className="fa-solid fa-star"></i>
-        </li>
-      </ul>
     </div>
   );
 }
