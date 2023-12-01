1590f9c502f65b857439542ae6f590743ffbe75c
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-12-01 16:31:13 +0200

    Create Details component and  SCSS

---
+++ SPA/src/components/details/deatails.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { cleanup, render, screen } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "../../contexts/authContext";
+import Details from "../Details";
+
+
+test("If clink button works", () => {
+  const { getByText } = render(
+    <BrowserRouter>
+      <AuthProvider>
+        <Details/>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+  const btn = screen.getByText("Back");
+
+  expect(btn).toBeInTheDocument();
+  expect(getByText("Back").href).toBe("http://localhost/catalog");
+});
