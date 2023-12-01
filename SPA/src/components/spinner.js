d8a0a2f3c9354b5c727ed409eafa1eb02122c5e4
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2023-12-01 16:34:56 +0200

    Create Spinner component

---
+++ SPA/src/components/spinner.js
@@ -0,0 +1,21 @@
+import "./spinner.module.scss";
+
+ const Spinner = () => {
+  return (
+    <div className="center">
+      Loading...
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+    </div>
+  );
+};
+
+export default Spinner
