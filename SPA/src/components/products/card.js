1ea2e7fcd039e026fbb312c59f4dd36dc5069b97
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2024-05-19 11:24:07 +0300

    Update card.js

--- SPA/src/components/products/card.js
+++ SPA/src/components/products/card.js
@@ -1,19 +1,19 @@
 import { routes } from "../../helpers/routeConfig";
 import styles from "./card.module.scss";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Card = (props) => {
   return (
     <div key={props.data._id} className={styles["card"]}>
-      <div className={styles["box-image"]}>
+      <div className={styles["box__image"]}>
         <img src={props.data.imageUrl} alt="...comics" />
       </div>
-      <div className={styles["card-body"]}>
-        <h2 className={styles["card-title"]}>{props.data.title}</h2>
+      <div className={styles["card__body"]}>
+        <h2 className={styles["card__title"]}>{props.data.title}</h2>
         <div>
-          <NavLink to={`${routes.details}/${props.data._id}`}>
+          <Link to={`${routes.details}/${props.data._id}`}>
             Read More
-          </NavLink>
+          </Link>
         </div>
       </div>
     </div>
